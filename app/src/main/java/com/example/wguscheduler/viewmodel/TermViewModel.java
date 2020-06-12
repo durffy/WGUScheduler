f18335fca30099375587fb17a2c6d5d91bc2d1d7
Connor Duffy <connor_duffy@hotmail.com>
2020-06-12 07:47:19 -0700

    Changing the inheritance

--- app/src/main/java/com/example/wguscheduler/viewmodel/TermViewModel.java
+++ app/src/main/java/com/example/wguscheduler/viewmodel/TermViewModel.java
@@ -2,21 +2,21 @@ package com.example.wguscheduler.viewmodel;
 
 import android.app.Application;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.example.wguscheduler.entities.TermEntity;
 
 import java.util.List;
 
-public class TermViewModel extends ViewModel {
+public class TermViewModel extends AndroidViewModel {
     private LiveData<List<TermEntity>> mAllTerms;
 
     public TermViewModel(Application application){
         super(application);
     }
 
-    public LiveData<Object> getAllTerms() {
+    public LiveData<List<TermEntity>> getAllTerms() {
         return mAllTerms;
     }
 }
