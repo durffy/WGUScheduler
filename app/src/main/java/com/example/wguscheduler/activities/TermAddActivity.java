e4be1f78db5dc478dfa2fc450ae96879a85ee738
Connor Duffy <connor_duffy@hotmail.com>
2020-06-23 08:07:49 -0700

    - creating TermAddActivity and related layouts

---
+++ app/src/main/java/com/example/wguscheduler/activities/TermAddActivity.java
@@ -0,0 +1,18 @@
+package com.example.wguscheduler.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wguscheduler.R;
+
+class TermAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_add);
+    }
+
+
+}
