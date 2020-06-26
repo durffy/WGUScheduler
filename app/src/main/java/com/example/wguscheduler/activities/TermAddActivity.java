44eedce2513101b5e7e7ab7dedfc25d431855332
cjd <connor_duffy@hotmail.com>
2020-06-26 07:22:59 -0700

    - adding onBackPress Support

--- app/src/main/java/com/example/wguscheduler/activities/TermAddActivity.java
+++ app/src/main/java/com/example/wguscheduler/activities/TermAddActivity.java
@@ -3,6 +3,7 @@ package com.example.wguscheduler.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.wguscheduler.R;
 
@@ -10,9 +11,20 @@ public class TermAddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_add);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
-
+    // add support for going back a screen
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
 }
