2c5006cb48b355732aac0f4bcc85fb8e6b99e3f5
cjd <connor_duffy@hotmail.com>
2020-06-29 07:50:51 -0700

    - adding the CourseAddActivity

---
+++ app/src/main/java/com/example/wguscheduler/activities/CourseAddActivity.java
@@ -0,0 +1,17 @@
+package com.example.wguscheduler.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wguscheduler.R;
+
+public class CourseAddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses_add);
+    }
+}
