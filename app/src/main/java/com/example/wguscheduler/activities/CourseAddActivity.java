ef1070188f6b8c553b384453bce2e575311f0426
cjd <connor_duffy@hotmail.com>
2020-06-29 08:01:49 -0700

    - adding on back press support

--- app/src/main/java/com/example/wguscheduler/activities/CourseAddActivity.java
+++ app/src/main/java/com/example/wguscheduler/activities/CourseAddActivity.java
@@ -5,6 +5,7 @@ import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.wguscheduler.R;
 
@@ -13,5 +14,19 @@ public class CourseAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses_add);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
+
+    // add support for going back a screen
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+
 }
