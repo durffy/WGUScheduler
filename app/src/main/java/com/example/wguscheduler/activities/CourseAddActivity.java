a89b95827d68d1126f10acbe9b61b4f3a0099ed8
cjd <connor_duffy@hotmail.com>
2020-07-02 07:25:53 -0700

    # ISSUE (solved) - private MentorEntity mMentor never gets a value assigned E/AndroidRuntime: FATAL EXCEPTION: main     Process: com.example.wguscheduler, PID: 18827     java.lang.NullPointerException: Attempt to invoke virtual method 'int com.example.wguscheduler.entities.MentorEntity.getId()' on a null object reference         at com.example.wguscheduler.activities.CourseAddActivity.saveCourse(CourseAddActivity.java:79)         at com.example.wguscheduler.activities.CourseAddActivity.access$000(CourseAddActivity.java:28)         at com.example.wguscheduler.activities.CourseAddActivity$1.onClick(CourseAddActivity.java:64)         at android.view.View.performClick(View.java:7438)         at android.view.View.performClickInternal(View.java:7415)         at android.view.View.access$3600(View.java:810)         at android.view.View$PerformClick.run(View.java:28286)         at android.os.Handler.handleCallback(Handler.java:938)         at android.os.Handler.dispatchMessage(Handler.java:99)         at android.os.Looper.loop(Looper.java:223)         at android.app.ActivityThread.main(ActivityThread.java:7523)         at java.lang.reflect.Method.invoke(Native Method)         at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:592)         at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:941)
    # SHOULD
    - mMentor should get the email
    
    # SUMMARY
    - changing if then to stream, testing, failed
    - modifying stream to assign the new MentorEntity if it doesnt exist, testing failed
    - calling saveMentor(mMentor) to save the mentor after the mMentor assignment, testing failed
    - - error, couldn't get the ID of the new MentorEntity, doesnt exist
    - adding mentors.size() + 1 to the creation of the MentorEntity, testing failed
    - setting up debugging for values
    - debugging log not showing anything, possible issue with assignment?
    - adding .orElse(mMentor = new MentorEntity())
    - - testing for new mentor, SUCCESS
    - - testing for existing mentor, SUCCESS

--- app/src/main/java/com/example/wguscheduler/activities/CourseAddActivity.java
+++ app/src/main/java/com/example/wguscheduler/activities/CourseAddActivity.java
@@ -2,32 +2,38 @@ package com.example.wguscheduler.activities;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.wguscheduler.R;
 import com.example.wguscheduler.entities.CourseEntity;
 import com.example.wguscheduler.entities.MentorEntity;
+import com.example.wguscheduler.entities.TermEntity;
 import com.example.wguscheduler.viewmodel.CourseViewModel;
 import com.example.wguscheduler.viewmodel.MentorViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CourseAddActivity extends AppCompatActivity {
 
+    private static final String TAG = "CourseAddActivity";
     private CourseViewModel mCourseViewModel;
     private MentorViewModel mMentorViewModel;
     private EditText mCourseTitle, mCourseStartDate, mCourseEndDate,
         mMentorFirstName, mMentorLastName, mMentorEmail, mMentorPhone;
-
+    private MentorEntity mMentor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,14 +71,13 @@ public class CourseAddActivity extends AppCompatActivity {
         });
     }
 
-    //(int mTermId, int mMentorId, String mTitle, String mStatus, Date mStartDate, Date mEndDate)
     private void saveCourse() throws ParseException{
 
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-        MentorEntity mentor = verifyMentor();
+        verifyMentor();
         int termId = getIntent().getIntExtra("termId", 0);
         //int mentorId = mentor.getMentorId();
-        int mentorId = 1;
+        int mentorId = mMentor.getId();
         String title = mCourseTitle.getText().toString();
         String status = "Plan to Take";
         Date start = formatter.parse(mCourseStartDate.getText().toString());
@@ -83,9 +88,29 @@ public class CourseAddActivity extends AppCompatActivity {
         onSupportNavigateUp();
     }
 
-    private MentorEntity verifyMentor() {
-        MentorEntity mentor = null;
-        return mentor;
+
+    private void verifyMentor() {
+
+        mMentorViewModel.getAllMentors().observe(this, new Observer<List<MentorEntity>>() {
+            @Override
+            public void onChanged(@Nullable final List<MentorEntity> mentors) {
+                String first = mMentorFirstName.getText().toString();
+                String last = mMentorLastName.getText().toString();
+                String phone = mMentorPhone.getText().toString();
+                String email = mMentorEmail.getText().toString();
+
+                //filter by email, emails should be unique to each person
+                mMentor = mentors.stream().filter(m -> m.getEmail().contains(email))
+                        .findFirst().orElse(mMentor = new MentorEntity(mentors.size() + 1, first, last, phone, email));
+                Log.d(TAG, "VerifyMentor().onChanged(): "+ mMentor.getId());
+                saveMentor(mMentor);
+            }
+        });
+
+    }
+
+    private void saveMentor(MentorEntity mentorEntity) {
+        mMentorViewModel.saveMentor(mentorEntity);
     }
 
     // add support for going back a screen
