3e231ffbc449639f2c9f71db5244f983f7ed3a1b
cjd <connor_duffy@hotmail.com>
2020-07-03 13:20:48 -0700

    - modifying elements of the activity

--- app/src/main/java/com/example/wguscheduler/activities/AssessmentAddActivity.java
+++ app/src/main/java/com/example/wguscheduler/activities/AssessmentAddActivity.java
@@ -1,13 +1,22 @@
 package com.example.wguscheduler.activities;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.wguscheduler.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssessmentAddActivity extends AppCompatActivity {
+
+    private EditText mEditTitle, mEditNotes, mScheduledDate;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,10 @@ public class AssessmentAddActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //init the elements
+        mEditTitle = findViewById(R.id.edit_assessment_add_title);
+        mEditNotes = findViewById(R.id.edit_assessment_add_notes);
 
 
 
