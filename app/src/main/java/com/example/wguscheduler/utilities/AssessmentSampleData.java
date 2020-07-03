c49b6d284652774b9adc0e3037f918cc2ac76b75
cjd <connor_duffy@hotmail.com>
2020-07-03 13:59:53 -0700

    - modifying to match the new constructor

--- app/src/main/java/com/example/wguscheduler/utilities/AssessmentSampleData.java
+++ app/src/main/java/com/example/wguscheduler/utilities/AssessmentSampleData.java
@@ -3,6 +3,7 @@ package com.example.wguscheduler.utilities;
 import com.example.wguscheduler.entities.AssessmentEntity;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +11,18 @@ public class AssessmentSampleData {
     public static List<AssessmentEntity> getAssessments() throws ParseException {
         List<AssessmentEntity> assessments = new ArrayList<>();
         AssessmentEntity assessment;
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 
-        assessment = new AssessmentEntity(1, 1, "Assessment", "Performance Based Exam", "Performance");
+        assessment = new AssessmentEntity(1, 1, "Assessment",
+                "Performance Based Exam", formatter.parse("01/30/2020"));
         assessments.add(assessment);
 
-        assessment = new AssessmentEntity(2, 1, "Assessment", "Objective Based Exam", "Objective");
+        assessment = new AssessmentEntity(2, 1, "Assessment",
+                "Objective Based Exam", formatter.parse("02/28/2020"));
         assessments.add(assessment);
 
-        assessment = new AssessmentEntity(3, 2, "Assessment", "Performance Based Exam", "Performance");
+        assessment = new AssessmentEntity(3, 2, "Assessment",
+                "Performance Based Exam", formatter.parse("07/30/2020"));
         assessments.add(assessment);
 
         return assessments;
