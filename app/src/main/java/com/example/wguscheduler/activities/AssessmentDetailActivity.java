eee9dc7cdc95e3bfb067878f3736073106289eda
cjd <connor_duffy@hotmail.com>
2020-07-07 07:18:20 -0700

    - loading details changed to an observer

--- app/src/main/java/com/example/wguscheduler/activities/AssessmentDetailActivity.java
+++ app/src/main/java/com/example/wguscheduler/activities/AssessmentDetailActivity.java
@@ -3,17 +3,28 @@ package com.example.wguscheduler.activities;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.wguscheduler.R;
+import com.example.wguscheduler.entities.AssessmentEntity;
+import com.example.wguscheduler.entities.CourseEntity;
+import com.example.wguscheduler.viewmodel.AssessmentViewModel;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class AssessmentDetailActivity extends AppCompatActivity{
+
+    private AssessmentViewModel mAssessmentViewModel;
     private TextView textViewAssessmentTitle,
         textViewAssessmentNotes,
         textViewAssessmentScheduledDate;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_detail);
@@ -35,13 +46,26 @@ public class AssessmentDetailActivity extends AppCompatActivity{
     }
 
     private void loadAssessmentDetails() {
+        mAssessmentViewModel = new ViewModelProvider(this).get(AssessmentViewModel.class);
+
         textViewAssessmentTitle = findViewById(R.id.text_assessment_title);
         textViewAssessmentScheduledDate = findViewById(R.id.text_assessment_scheduled_date_output);
         textViewAssessmentNotes = findViewById(R.id.text_assessment_notes_output);
-        if(getIntent().getStringExtra("title") != null){
-            textViewAssessmentTitle.setText(getIntent().getStringExtra("title"));
-            textViewAssessmentScheduledDate.setText(getIntent().getStringExtra("scheduledDate"));
-            textViewAssessmentNotes.setText(getIntent().getStringExtra("notes"));
+
+        if(mAssessmentViewModel.getAllAssessments() != null) {
+            mAssessmentViewModel.getAllAssessments().observe(this, new Observer<List<AssessmentEntity>>() {
+                @Override
+                public void onChanged(@Nullable final List<AssessmentEntity> assessments) {
+                    SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+                    for (AssessmentEntity a : assessments) {
+                        if(a.getId()== getIntent().getIntExtra("assessmentId", 0)){
+                            textViewAssessmentTitle.setText(a.getTitle());
+                            textViewAssessmentScheduledDate.setText(formatter.format(a.getScheduledDate()));
+                            textViewAssessmentNotes.setText(a.getNotes());
+                        }
+                    }
+                }
+            });
         }
     }
 
