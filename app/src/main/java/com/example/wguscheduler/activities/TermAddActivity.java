aea77c4110c12f8bf7f65fc777e565d61818b6b8
cjd <connor_duffy@hotmail.com>
2020-07-09 11:33:02 -0700

    - added date picker to TermAddActivity

--- app/src/main/java/com/example/wguscheduler/activities/TermAddActivity.java
+++ app/src/main/java/com/example/wguscheduler/activities/TermAddActivity.java
@@ -1,8 +1,10 @@
 package com.example.wguscheduler.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +19,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class TermAddActivity extends AppCompatActivity {
 
     private EditText mTermTitle, mTermStart, mTermEnd;
     private TermViewModel mTermViewModel;
+    private Calendar mEndCalendar = Calendar.getInstance();
+    private Calendar mStartCalendar = Calendar.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +50,14 @@ public class TermAddActivity extends AppCompatActivity {
 
         //initialize the view model
         mTermViewModel = new ViewModelProvider(this).get(TermViewModel.class);
-        
+
+        try {
+            loadStartDatePicker();
+            loadEndDatePicker();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,4 +87,61 @@ public class TermAddActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
+
+    private void loadStartDatePicker() throws ParseException {
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mStartCalendar.set(Calendar.YEAR, year);
+                mStartCalendar.set(Calendar.MONTH, monthOfYear);
+                mStartCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateStartLabel();
+
+            }
+        };
+
+        mTermStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(TermAddActivity.this,date, mStartCalendar.get(Calendar.YEAR), mStartCalendar.get(Calendar.MONTH),
+                        mStartCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateStartLabel() {
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        mTermStart.setText(sdf.format(mStartCalendar.getTime()));
+
+    }
+
+    private void loadEndDatePicker() throws ParseException {
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mEndCalendar.set(Calendar.YEAR, year);
+                mEndCalendar.set(Calendar.MONTH, monthOfYear);
+                mEndCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateEndLabel();
+            }
+        };
+
+        mTermEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(TermAddActivity.this,date, mEndCalendar.get(Calendar.YEAR), mEndCalendar.get(Calendar.MONTH),
+                        mEndCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+        });
+    }
+
+    private void updateEndLabel() {
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        mTermEnd.setText(sdf.format(mEndCalendar.getTime()));
+    }
+
 }
